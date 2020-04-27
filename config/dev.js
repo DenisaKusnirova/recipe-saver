8f5249c8a54b20f1714113392dcf59a1f2fbeb1c
DenisaKusnirova <deniskakusnirova@gmail.com>
2020-04-27 12:06:27 +0200

    no message

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    googleClientID:
-        "234573320635-to08nb87h1gjkm69q5j4ut955r9frmfs.apps.googleusercontent.com",
-    googleClientSecret: "97Wjwnl-SaumMFQWm_BhwaiC",
-    facebookClientID: "566410414229115",
-    facebookClientSecret: "688b4971c1c06478a931742de9af1218",
-    mongoURI: "mongodb+srv://deny:sgo3JIN7M8BvA2vC@recipe-saver-5hx2d.mongodb.net/test?retryWrites=true&w=majority",
-    cookieKey: "ewrelwkrj3l2iu23br",
-};
